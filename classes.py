ba4d6b6a04207c882011d798c3f7a0a35d62c5be
Josh Friedlander <joebobson87@yahoo.com>
2020-01-28 19:28:58 +0200

    refine structure

--- classes.py
+++ classes.py
@@ -1,19 +1,36 @@
-#TODO
-# should there be a korban parent that everything inherits from?
-# kodsheikodshim/kalim - hierarchy doesn't make sense
-# consts
-M = 'male'
-F = 'female'
-GOAT = 'goat'
-SHEEP = 'sheep'
-BIRD = 'bird'
-COW = 'cow'
-KODSHEI_KODSHIM = 'kodshei_kodshim'
-KODSHIM_KALIM = 'kodshim_kalim'
-N = 'north'
-ANY = 'anywhere in Azara'
+import names as n
 
-class AnimalKorban:
+# kodsheikodshim/kalim - hierarchy doesn't make sense
+
+class Hekdesh:
+    def __init__(self):
+        self.has_meila = True
+        self.redeemable = None
+
+    def redeem(self):
+        if self.redeemable:
+            return "Redeemable, add a fifth (external)"
+        else:
+            return "Not redeemable"
+
+
+class KedushasDamim(Hekdesh):
+    def __init__(self):
+        self.redeemable = True
+
+
+class KedushasHaguf(Hekdesh):
+    def __init__(self, has_mum=False):
+        if not has_mum:
+            self.redeemable = False
+
+
+class OrganicKorban(KedushasHaguf):
+    def __init__(self):
+        self.needs_kedushas_kli = True
+
+
+class AnimalKorban(KedushasHaguf):
     def __init__(self, species, gender):
         self.species = species
         self.gender = gender
@@ -21,8 +38,8 @@ class AnimalKorban:
 
 class KodsheiKodshim(AnimalKorban):
     def __init__(self, species, gender):
-        self.place_slaughtered = N
-        self.place_blood_received = N
+        self.place_slaughtered = n.NORTH
+        self.place_blood_received = n.NORTH
         self.matanot_disqualify_if_missing = 'any'
         self.place_remainder_sprinkled = 'Altar base (West)'
         self.remainder_pouring_disqualifies = False
@@ -31,8 +48,8 @@ class KodsheiKodshim(AnimalKorban):
 
 class KodshimKalim(AnimalKorban):
     def __init__(self, species, gender):
-        self.place_slaughtered = ANY
-        self.place_blood_received = ANY
+        self.place_slaughtered = n.ANY
+        self.place_blood_received = n.ANY
         self.matanot_disqualify_if_missing = 'any'
         self.place_remainder_sprinkled = 'Altar base (West)'
         self.remainder_pouring_disqualifies = False
@@ -41,8 +58,8 @@ class KodshimKalim(AnimalKorban):
 
 class Olah(KodsheiKodshim):
     def __init__(self, species, gender):
-        assert species in [BIRD, COW, SHEEP, GOAT], 'Olah must be bird, cow, sheep or goat'
-        assert gender == M, 'Olah must be male'
+        assert species in [n.BIRD, n.COW, n.SHEEP, n.GOAT], 'Olah must be bird, cow, sheep or goat'
+        assert gender == n.MALE, 'Olah must be male'
         self.eaten = False
         super().__init__(species=species, gender=gender)
 
@@ -52,7 +69,7 @@ class Mincha:
 
 class Shlamim(KodshimKalim):
     def __init__(self, species, gender):
-        assert species in [COW, SHEEP, GOAT], 'Shlamim must be cow, sheep or goat'
+        assert species in [n.COW, n.SHEEP, n.GOAT], 'Shlamim must be cow, sheep or goat'
         super().__init__(species=species, gender=gender)
 
 
@@ -74,24 +91,24 @@ class OuterChatas(Chatas):
 
 class Seirhamishtaleach:
     def __init__(self):
-        self.species = GOAT
-        self.gender = M
+        self.species = n.GOAT
+        self.gender = n.MALE
 
 #tamid
-morning_tamid = Olah(SHEEP, M)
-afternoon_tamid = Olah(SHEEP, M)
+morning_tamid = Olah(n.SHEEP, n.MALE)
+afternoon_tamid = Olah(n.SHEEP, n.MALE)
 
 # musaf
-par_musaf = Olah(COW, M)
-ayil_musaf = Olah(GOAT, M)  # this *is* the ayil of vayikra 16:5!
-kivsei_musaf = [Olah(SHEEP, M) for k in range(1, 7)]
-seir_musaf = OuterChatas(GOAT, M)
+par_musaf = Olah(n.COW, n.MALE)
+ayil_musaf = Olah(n.GOAT, n.MALE)  # this *is* the ayil of vayikra 16:5!
+kivsei_musaf = [Olah(n.SHEEP, n.MALE) for k in range(1, 7)]
+seir_musaf = OuterChatas(n.GOAT, n.MALE)
 
 # korbanos hayom
-par_kohen_gadol = InnerChatas(COW, M)
-ayil_kohen_gadol = Olah(GOAT, M)
+par_kohen_gadol = InnerChatas(n.COW, n.MALE)
+ayil_kohen_gadol = Olah(n.GOAT, n.MALE)
 
-seir_haam = InnerChatas(GOAT, M)
+seir_haam = InnerChatas(n.GOAT, n.MALE)
 seir_hamishtaleyach = Seirhamishtaleach()
 
 
@@ -108,3 +125,4 @@ seir_hamishtaleyach = Seirhamishtaleach()
 #
 #
 ## https://he.wikisource.org/wiki/%D7%A1%D7%A4%D7%A8%D7%90_%D7%A2%D7%9C_%D7%95%D7%99%D7%A7%D7%A8%D7%90_%D7%90#%D7%A4%D7%A1%D7%95%D7%A7_%D7%99%D7%91_(%D7%9B%D7%9C_%D7%94%D7%A4%D7%A8%D7%A7)(%D7%9B%D7%9C_%D7%94%D7%A4%D7%A1%D7%95%D7%A7)
+print('hello world')
